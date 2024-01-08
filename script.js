e6804a14e43fbc47cab7ee260fc9a0eb87814206
Liz <elizabeth.pinero.r@gmail.com>
2024-01-07 20:38:57 -0500

    Added & animated images

--- script.js
+++ script.js
@@ -13,21 +13,33 @@ const enemiesArray = []
 //     height: 200,
 // }
 
+const enemyImage = new Image()
+enemyImage.src = 'enemy1.png'
+let gameFrame = 0
+
 class Enemy {
     constructor() {
         this.x = Math.random() * canvas.width
         this.y = Math.random() * canvas.height
-        this.width = 100
-        this.height = 100
         // random num between -2 and +2
         this.speed = Math.random() * 4 - 2 
+        this.spriteWidth = 293
+        this.spriteHeight = 155
+        this.width = this.spriteWidth / 2.5
+        this.height = this.spriteHeight / 2.5
+        this.frame = 0
     }
     update() {
         this.x += this.speed
         this.y += this.speed
+        // animate sprites
+        // Only run this code every 2 loops
+        if(gameFrame % 2 === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++
+        }
     }
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
 
@@ -50,7 +62,7 @@ function animate() {
         enemy.update()
         enemy.draw()
     })
-
+    gameFrame++
     requestAnimationFrame(animate)
 }
 animate()
