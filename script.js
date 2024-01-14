2a82ee19aefa192eaf465e1e9d580c78ff0a9a3b
Liz <elizabeth.pinero.r@gmail.com>
2024-01-14 14:05:40 -0500

    Updated new creature path

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /**@type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1')
+const canvas2 = document.getElementById('canvas2')
+
 const ctx = canvas.getContext('2d')
+const ctx2 = canvas.getContext('2d')
 CANVAS_WIDTH = canvas.width = 500
 CANVAS_HEIGHT = canvas.height = 1000
-const numberOfEnemies = 20
+const numberOfEnemies = 200
 const enemiesArray = []
 
 // enemy1 = {
@@ -17,26 +20,26 @@ let gameFrame = 0
 class Enemy {
     constructor() {
         this.image = new Image()
-        this.image.src = 'enemy2.png'
+        this.image.src = 'enemy3.png'
         // random num between -2 and +2
         this.speed = Math.random() * 4 + 1
-        this.spriteWidth = 266
-        this.spriteHeight = 188
-        this.width = this.spriteWidth / 2.5
-        this.height = this.spriteHeight / 2.5
+        this.spriteWidth = 218
+        this.spriteHeight = 177
+        this.width = this.spriteWidth / 2
+        this.height = this.spriteHeight / 2
         this.x = Math.random() * (canvas.width - this.width)
         this.y = Math.random() * (canvas.height - this.height)
 
         this.frame = 0
         // flaps randomly at speed between 1 and 4
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
-        this.angle = Math.random() * 2
-        this.angleSpeed = Math.random() * 0.2
-        this.curve = Math.random() * 7
+        this.angle = 0 
+        this.angleSpeed = Math.random() * 2 + 0.5
+        this.curve = Math.random() * 200 + 50
     }
     update() {
-        this.x -= this.speed
-        this.y += this.curve * Math.sin(this.angle)
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + canvas.width/2 - this.width/2
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/270) + canvas.height/2 - this.height/2
         this.angle += this.angleSpeed
         // this.x += Math.random() * 5 - 2.5
         // this.y += Math.random() * 5 - 2.5
